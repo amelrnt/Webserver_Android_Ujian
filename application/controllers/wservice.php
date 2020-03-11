052b1caeb3254072c5cda9a46a59be2ab027ac2a
HattaMaulana <mahatta.maulana@gmail.com>
2020-03-11 21:17:19 +0700

    update readme.md

--- application/controllers/wservice.php
+++ application/controllers/wservice.php
@@ -1,5 +1,5 @@
 <?php
-class Wservice extends CI_Controller{
+class Wservice extends CI_Controller {
 
 	function CheckVersion(){
 		$this->load->model('adminmod');
@@ -13,22 +13,15 @@ class Wservice extends CI_Controller{
 		echo json_encode($this->wsmodel->getSoal($paket));
 	}
 
-	function updatesoal($paket){
+	function update_soal($paket){
 		$this->load->model('wsmodel');
 		$result['tbl_soal']=$this->wsmodel->getSoal($paket);
 		echo json_encode($result);
 	}
-
-	// function getFile($paket){
-	// 	$this->load->model('wsmodel');
-	// 	$result['filename']=$this->wsmodel->getFileGambar($paket);
-	// 	echo json_encode($result);
-	// }
 
 	function updatePaket(){
 		$this->load->model('adminmod');
 		$result['tbl_paket']=$this->adminmod->tampilData('tbl_paket');
 		echo json_encode($result);
 	}
-}
-?>
+} ?>
